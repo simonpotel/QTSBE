863febafe6dd697ee0bf0f02d7839e47cb5efc5b
simon <155122848+simonpotel@users.noreply.github.com>
2024-05-19 19:59:30 +0200

    RSI Indicator

---
+++ api/algo/indicators/rsi.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def get_rsi(prices, window=14):
+    deltas = np.diff(prices)
+    seed = deltas[:window+1]
+    up = seed[seed >= 0].sum() / window
+    down = -seed[seed < 0].sum() / window
+    rs = up / down
+    rsi = np.zeros_like(prices)
+    rsi[:window] = 100. - 100. / (1. + rs)
+
+    for i in range(window, len(prices)):
+        delta = deltas[i - 1]
+        if delta > 0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+
+        up = (up * (window - 1) + upval) / window
+        down = (down * (window - 1) + downval) / window
+
+        rs = up / down
+        rsi[i] = 100. - 100. / (1. + rs)
+
+    return rsi
