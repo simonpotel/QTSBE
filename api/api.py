49b69a52f15db1741eaca1cd779d7d66d891f333
simon <155122848+simonpotel@users.noreply.github.com>
2024-05-19 18:05:46 +0200

    introducing loguru in project

--- api/api.py
+++ api/api.py
@@ -1,30 +1,39 @@
 import os
 import sys 
-
-from flask import Flask, jsonify, request, make_response
-from flask_cors import CORS
-print(os.getcwd())
-
 # Add parent directories to sys.path to import modules
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
-
+from loguru import logger
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
 from DEXcryptoLib.Lib import *
 from algo.algo import *
 
+debug_mode = True
 
 app = Flask(__name__)
 CORS(app, resources={r"/QTSBE/*": {"origins": "http://127.0.0.1"}})
-
 @app.route('/QTSBE/<pair>/<algo>')
 def get_data(pair, algo):
     response = jsonify(
         {"pair": pair, 
-         "algo": algo}
+         "data": algo}
         )
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    logger.info(f"Request pair: {pair} | algo: {algo}")
+    logger.debug(f"Request response: {response}")
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    logger.remove()
+    if debug_mode:
+        logger.add("logs\debug.log", level="DEBUG")
+        logger.add(sys.stdout, level="DEBUG")  
+    else:
+        logger.add("logs\logs.log", level="INFO")  
+        logger.add(sys.stdout, level="INFO")  
+
+    logger.warning("API has been restarted.")
+    app.run(debug=debug_mode)
+
