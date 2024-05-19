046f01b5513e698fa4fa8520f7d5ccd3e554bca2
simon <155122848+simonpotel@users.noreply.github.com>
2024-05-19 17:49:12 +0200

    algo class, DEXcryptoLib, logo, api flusk

--- api/algo/algo.py
+++ api/algo/algo.py
@@ -1,1 +1,57 @@
-# class algo here
+import os
+import sys
+import requests
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+
+# import the lib DEXcryptoLib
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+from DEXcryptoLib.Lib import *
+
+class algo(object):
+    def __init__(self,
+                 name="",
+                 version="",
+                 author="",
+                 description="",
+                 db_conntect=True):
+        self.name = name
+        self.version = version 
+        self.author = author 
+        self.description = description
+        if db_conntect:
+            self.db_smartswap_data = init_database(
+                "smartswap_data",
+                os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../configs/db_config.json")
+                )
+            self.db_smartswap_data.connect()
+
+    def get_crypto_logo_from_table_str(self, text):
+        tokens = text.split("_")
+        last_logo_link = None
+        for token in tokens:
+            logo_link = self.get_crypto_logo(token)
+            if logo_link:
+                last_logo_link = logo_link
+        return last_logo_link
+
+    def get_crypto_logo(self, token_name, width=None, height=None):
+        url = "https://cryptologos.cc/logos/"
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                logo_links = soup.find_all("a", string=lambda text: token_name.lower() in text.lower())
+                if logo_links:
+                    logo_link = logo_links[0]['href']
+                    if width and height:
+                        logo_link += f"?width={width}&height={height}"
+                    return logo_link
+                else:
+                    return None 
+            else:
+                print("The request failed.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
--- api/api.py
+++ api/api.py
@@ -1,1 +1,30 @@
-# flusk system here
+import os
+import sys 
+
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
+print(os.getcwd())
+
+# Add parent directories to sys.path to import modules
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
+from DEXcryptoLib.Lib import *
+from algo.algo import *
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/QTSBE/*": {"origins": "http://127.0.0.1"}})
+
+@app.route('/QTSBE/<pair>/<algo>')
+def get_data(pair, algo):
+    response = jsonify(
+        {"pair": pair, 
+         "algo": algo}
+        )
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
