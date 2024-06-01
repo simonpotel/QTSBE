91d22d4220f179d2f117a69233e66e5784218adb
simon <155122848+simonpotel@users.noreply.github.com>
2024-06-01 17:54:01 +0200

    plotly interface for cuddles

---
+++ display/web/main.py
@@ -0,0 +1,44 @@
+import os
+import customtkinter as ctk
+from result_gui import fetch_and_show_data
+
+def list_files_in_directory(directory):
+    """Get files of a directory without their extension"""
+    try:
+        return [os.path.splitext(f)[0] for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    except:
+        return ['None']
+
+ctk.set_appearance_mode("dark")  # "light", "dark", "system"
+ctk.set_default_color_theme("dark-blue")  # "blue", "dark-blue", "green"
+
+# create the main window QTSBE
+root = ctk.CTk()
+root.title("QTSBE")
+
+root.geometry("420x185")
+
+# pack grid for the items
+frame = ctk.CTkFrame(root, corner_radius=10)
+frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+# data selector
+label_data = ctk.CTkLabel(frame, text="Data:")
+label_data.grid(row=0, column=0, pady=(10, 0), padx=(10, 0), sticky="e")
+data_combo = ctk.CTkComboBox(frame, values=list_files_in_directory("data/bank/"), width=300)
+data_combo.grid(row=0, column=1, pady=(10, 0), padx=10, sticky="ew")
+
+# strategy selector
+label_strategy = ctk.CTkLabel(frame, text="Strategy:")
+label_strategy.grid(row=1, column=0, pady=(10, 0), padx=(10, 0), sticky="e")
+strategy_combo = ctk.CTkComboBox(frame, values=list_files_in_directory("api/strategies/"), width=300)
+strategy_combo.grid(row=1, column=1, pady=(10, 0), padx=10, sticky="ew")
+
+# button that make a request in result_gui.py to the API and print the data
+load_button = ctk.CTkButton(frame, text="Load", command=lambda: fetch_and_show_data(data_combo, strategy_combo))
+load_button.grid(row=2, column=0, columnspan=2, pady=(20, 10))
+
+frame.columnconfigure(0, weight=1)
+frame.columnconfigure(1, weight=3)
+
+root.mainloop()
---
+++ display/web/result_gui.py
@@ -0,0 +1,116 @@
+import requests
+import customtkinter as ctk
+import json
+from datetime import datetime
+import plotly.graph_objs as go
+import webbrowser
+import os
+from plotly.subplots import make_subplots
+
+chart_colors = {
+    "price": "#6c7386", #gunmetal
+    "mm_100": "#B8336A", #raspberry rose
+    "mm_40": "#FF9B42", #sandy brown
+    "mm_20": "#00A7E1", #picton blue
+    "test": "#C73E1D", #sinopia
+    "rsi": "#9AB87A", #olivine
+}
+
+def fetch_and_show_data(data_combo, strategy_combo):
+    data_file = data_combo.get()
+    strategy = strategy_combo.get()
+    url = f"http://127.0.0.1:5000/QTSBE/{data_file}/{strategy}"
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        display_results_in_new_window(response.text, data_combo, strategy_combo, url)  
+    except requests.RequestException as e:
+        print("Request failed:", e)
+
+def display_results_in_new_window(response_text, data_combo, strategy_combo, url):
+    """display the fetched results in a new window with a plot and JSON data"""
+    new_window = ctk.CTkToplevel()  
+    new_window.title("Result: " + url)
+    new_window.geometry("1200x800")
+
+    text_frame, graph_frame = create_frames(new_window)
+
+    json_data = json.loads(response_text)
+    display_json_data(json_data, text_frame)
+    add_save_button(text_frame, json_data)
+
+    plot_json_data_in_gui(json_data, graph_frame, data_combo, strategy_combo)
+
+def create_frames(new_window):
+    """create and pack the text and graph frames in the new window."""
+    text_frame = ctk.CTkFrame(new_window, corner_radius=10)
+    text_frame.pack(fill="both", expand=True, side="left")
+    graph_frame = ctk.CTkFrame(new_window, corner_radius=10)
+    graph_frame.pack(fill="both", expand=True, side="right")
+    return text_frame, graph_frame
+
+def display_json_data(json_data, text_frame):
+    """display formatted JSON request data in a text widget."""
+    text_widget = ctk.CTkTextbox(text_frame, wrap="none")
+    text_widget.pack(fill="both", expand=True)
+    formatted_json = json.dumps(json_data, indent=4)
+    text_widget.insert("1.0", formatted_json)
+
+def add_save_button(text_frame, json_data):
+    """add a button to save JSON data to a file."""
+    save_button = ctk.CTkButton(text_frame, text="Save Request Json", command=lambda: save_to_file(json_data))
+    save_button.pack(pady=10)
+
+def save_to_file(content):
+    """save JSON content to a file."""
+    try:
+        os.makedirs('display/python/saved_results/', exist_ok=True)
+        filename = generate_filename(content)
+        with open(filename, "w") as file:
+            file.write(json.dumps(content, indent=4))
+        print("Content saved")
+    except Exception as e:
+        print("Failed to save content:", e)
+
+def generate_filename(content):
+    """generate a filename based on the current timestamp, pair, and strategy."""
+    return f"display/python/saved_results/{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}_{content['pair']}_{content['strategy']}.json"
+
+def plot_json_data_in_gui(json_data, graph_frame, data_combo, strategy_combo):
+    """Plot JSON data in the GUI with candlestick chart and RSI chart if available."""
+    dates, opens, highs, lows, closes = extract_ohlc_data(json_data['data'])
+    indicators = extract_indicators(json_data)
+
+    if not dates or not opens or not highs or not lows or not closes:
+        print("Error: OHLC data is missing or empty")
+        return
+
+    # subplot grid
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.05)
+
+    # candlestick chart to the first subplot
+    fig.add_trace(go.Candlestick(x=dates, open=opens, high=highs, low=lows, close=closes), row=1, col=1)
+
+    # layout
+    fig.update_layout(title=f"{data_combo.get()} ({strategy_combo.get()})",
+                      xaxis_title='Date',
+                      yaxis_title='Price',
+                      xaxis_rangeslider_visible=False)
+
+    # plot
+    plot_filename = 'temp_plot.html'
+    fig.write_html(plot_filename)
+    webbrowser.open(plot_filename)
+
+def extract_ohlc_data(data):
+    """Extract OHLC data from the JSON data."""
+    dates, opens, highs, lows, closes, volume = zip(*data)
+    return dates, opens, highs, lows, closes
+
+def extract_indicators(json_data):
+    """Extract indicators data from the JSON data."""
+    indicators = {}
+    for indicator in json_data['result'][0]:
+        indicators[indicator] = json_data['result'][0][indicator]
+    return indicators
