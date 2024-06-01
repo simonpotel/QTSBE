271f01ebb3e59d4466b103aa976fb4664423f4ef
simon <155122848+simonpotel@users.noreply.github.com>
2024-06-01 12:05:55 +0200

    Default is now a template
    Edited default.py strategy to make it simpler to copy and past as a template to create a new one.

--- api/strategies/default.py
+++ api/strategies/default.py
@@ -2,50 +2,17 @@ from stats.trades import Positions
 from api import logger
 
 def analyse(data, prices):
-    logger.debug("No analyse (DEFAULT)")
-    positions = Positions() # You must create an object of the class positions
-    positions.indicators = {
-        # This is an example of indicator, but you can use the fonctions in algo/indicators/
-        'test': [price*2 for price in prices] 
+    positions = Positions()
+
+    positions.indicators = {  # must convert all to a list because its using np (Object of type ndarray is not JSON serializable)
     }
+
+    #for i in range(len(prices) - 1):  # iterate through each price entry
+        #if indicator[i] is None: continue  # no data on a indicator (for the first data of prices)
+
+        #if positions.current_position == {}: # not already in a position
+            #
+        #else:
+            #
+
     return positions
-
-# structure of indicators:
-# {
-#"mm_20": [] ;tab of values
-#}
-
-# structure of trades (2 trades ex)
-#      {
-#        "buy": {
-#          "buy_date": "2021-01-04",
-#          "buy_price": 2.1789
-#        },
-#        "ratio": 1.3719307907659828,
-#        "sell": {
-#          "sell_date": "2021-01-22",
-#          "sell_price": 2.9893
-#        }
-#      },
-#      {
-#        "buy": {
-#          "buy_date": "2021-01-23",
-#          "buy_price": 3.3302
-#        },
-#        "ratio": 4.251576481892979,
-#        "sell": {
-#          "sell_date": "2021-03-16",
-#          "sell_price": 14.1586
-#        }
-#      },
-#      ...
-#      }
-
-# structure of position (number = buy_index = the index where it bought in prices tab)
-# ( if there is not any position then its a void dic : {} )
-#    {
-#      "1362": {
-#        "buy_date": "2024-05-04",
-#        "buy_price": 143.7
-#      }
-#    }
