59f23cf0f92e0abf9ca29ecb0d9222414616006e
simon <155122848+simonpotel@users.noreply.github.com>
2024-06-05 19:28:17 +0200

    Delete get_data.py

--- data/get_data.py
+++
@@ -1,8 +0,0 @@
-from binance_api import BinanceAPI
-
-BinanceAPI = BinanceAPI()
-#BinanceAPI.fetch_tokens_daily_ohlcv(BinanceAPI.get_top_50_tokens_by_volume())
-#BinanceAPI.fetch_tokens_daily_ohlcv(BinanceAPI.get_recent_try_pairs())
-#BinanceAPI.fetch_tokens_daily_ohlcv(BinanceAPI.get_least_volatile_tokens(15))
-#BinanceAPI.fetch_tokens_daily_ohlcv(BinanceAPI.get_most_volatile_tokens(2))
-#BinanceAPI.fetch_and_save_ohlcv("BTCUSDT", "1d")
