dffe54dd30b5364467d1ab93b8e6f91c70b4fe74
simon <155122848+simonpotel@users.noreply.github.com>
2024-06-05 12:18:06 +0200

    display/python strategies import with subdir

--- display/python/main.py
+++ display/python/main.py
@@ -2,12 +2,16 @@ import os
 import customtkinter as ctk
 from result_gui import fetch_and_show_data
 
-def list_files_in_directory(directory):
-    """Get files of a directory without their extension"""
-    try:
-        return [os.path.splitext(f)[0] for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    except:
-        return ['None']
+def list_files_in_directory(directory, extension):
+    """Get files of a directory and its subdirectories without their extension"""
+    files = []
+    for root, dirs, files_in_dir in os.walk(directory):
+        for file_name in files_in_dir:
+            if file_name.endswith(extension):  # Only consider files with the specified extension
+                relative_path = os.path.relpath(os.path.join(root, file_name), directory)
+                name_without_extension = os.path.splitext(relative_path)[0].replace(os.sep, '_')
+                files.append(name_without_extension)
+    return files or ['None']
 
 ctk.set_appearance_mode("dark")  # "light", "dark", "system"
 ctk.set_default_color_theme("dark-blue")  # "blue", "dark-blue", "green"
@@ -25,13 +29,13 @@ frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 # data selector
 label_data = ctk.CTkLabel(frame, text="Data:")
 label_data.grid(row=0, column=0, pady=(10, 0), padx=(10, 0), sticky="e")
-data_combo = ctk.CTkComboBox(frame, values=list_files_in_directory("data/bank/"), width=300)
+data_combo = ctk.CTkComboBox(frame, values=list_files_in_directory("data/bank/", ".csv"), width=300)
 data_combo.grid(row=0, column=1, pady=(10, 0), padx=10, sticky="ew")
 
 # strategy selector
 label_strategy = ctk.CTkLabel(frame, text="Strategy:")
 label_strategy.grid(row=1, column=0, pady=(10, 0), padx=(10, 0), sticky="e")
-strategy_combo = ctk.CTkComboBox(frame, values=list_files_in_directory("api/strategies/"), width=300)
+strategy_combo = ctk.CTkComboBox(frame, values=list_files_in_directory("api/strategies/", ".py"), width=300)
 strategy_combo.grid(row=1, column=1, pady=(10, 0), padx=10, sticky="ew")
 
 # button that make a request in result_gui.py to the API and print the data
