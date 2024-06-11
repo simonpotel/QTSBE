0643e68033570432552c67e1a36d89fc50a455a3
simon <155122848+simonpotel@users.noreply.github.com>
2024-06-11 15:49:03 +0200

    Simplify Position Code

--- api/stats/positions.py
+++ api/stats/positions.py
@@ -17,71 +17,40 @@ def get_position_stats(positions):
         'sell_signals_count': {}
     }
 
-    lowest_ratio = float('inf')
-    lowest_ratio_buy_index = None
-    lowest_ratio_sell_index = None
-    biggest_ratio = float('-inf')
-    biggest_ratio_buy_index = None
-    biggest_ratio_sell_index = None
-    total_ratio = 0
-    all_ratios = []
-    cumulative_ratios = []
-    max_cumulative_ratio = 1  # 1 (we start with no loss)
-    total_days = 0
-    total_positions = len(positions.positions)
-    if total_positions < 1:
+    if not positions.positions:
         return result_stats
+
+    total_ratio = 0
+    cumulative_ratios = []
+    total_days = 0
 
     for position in positions.positions:
         ratio = position['ratio']
-        all_ratios.append(ratio)
-        total_ratio += ratio
-
-        if ratio < lowest_ratio:
-            lowest_ratio = ratio
-            lowest_ratio_buy_index = position['buy_index']
-            lowest_ratio_sell_index = position['sell_index']
-
-        if ratio > biggest_ratio:
-            biggest_ratio = ratio
-            biggest_ratio_buy_index = position['buy_index']
-            biggest_ratio_sell_index = position['sell_index']
-
-        if not cumulative_ratios:
-            cumulative_ratios.append(ratio)
-        else:
-            cumulative_ratios.append(cumulative_ratios[-1] * ratio)
-        max_cumulative_ratio = cumulative_ratios[-1]
-
+        buy_index, sell_index = position['buy_index'], position['sell_index']
         buy_date = datetime.strptime(position['buy_date'], '%Y-%m-%d')
         sell_date = datetime.strptime(position['sell_date'], '%Y-%m-%d')
-        duration = sell_date - buy_date
-        total_days += duration.days
+        duration = (sell_date - buy_date).days
+        buy_signal = position['buy_signals']["Buy_Signal"]
+        sell_signal = position['sell_signals']["Sell_Signal"]
+
+        total_ratio += ratio
+        result_stats['all_ratios'].append(ratio)
+        cumulative_ratios.append(cumulative_ratios[-1] * ratio if cumulative_ratios else ratio)
+        total_days += duration
         
-        if position['buy_signals']["Buy_Signal"] not in result_stats['buy_signals_count']:
-            result_stats['buy_signals_count'][position['buy_signals']["Buy_Signal"]] = 1
-        else:
-            result_stats['buy_signals_count'][position['buy_signals']["Buy_Signal"]] += 1
+        if ratio < result_stats['lowest_ratio']:
+            result_stats['lowest_ratio'], result_stats['lowest_ratio_buy_index'], result_stats['lowest_ratio_sell_index'] = ratio, buy_index, sell_index
+        if ratio > result_stats['biggest_ratio']:
+            result_stats['biggest_ratio'], result_stats['biggest_ratio_buy_index'], result_stats['biggest_ratio_sell_index'] = ratio, buy_index, sell_index
 
-        if position['sell_signals']["Sell_Signal"] not in result_stats['sell_signals_count']:
-            result_stats['sell_signals_count'][position['sell_signals']["Sell_Signal"]] = 1
-        else:
-            result_stats['sell_signals_count'][position['sell_signals']["Sell_Signal"]] += 1
+        result_stats['buy_signals_count'][buy_signal] = result_stats['buy_signals_count'].get(buy_signal, 0) + 1
+        result_stats['sell_signals_count'][sell_signal] = result_stats['sell_signals_count'].get(sell_signal, 0) + 1
 
-
-    average_ratio = total_ratio / total_positions
-    average_position_duration = total_days / total_positions
-
-    result_stats['lowest_ratio'] = lowest_ratio
-    result_stats['lowest_ratio_buy_index'] = lowest_ratio_buy_index
-    result_stats['lowest_ratio_sell_index'] = lowest_ratio_sell_index
-    result_stats['biggest_ratio'] = biggest_ratio
-    result_stats['biggest_ratio_buy_index'] = biggest_ratio_buy_index
-    result_stats['biggest_ratio_sell_index'] = biggest_ratio_sell_index
-    result_stats['average_ratio'] = average_ratio
-    result_stats['all_ratios'] = all_ratios
-    result_stats['cumulative_ratios'] = cumulative_ratios
-    result_stats['max_cumulative_ratio'] = max_cumulative_ratio
-    result_stats['average_position_duration'] = average_position_duration
+    result_stats.update({
+        'average_ratio': total_ratio / len(positions.positions),
+        'cumulative_ratios': cumulative_ratios,
+        'max_cumulative_ratio': cumulative_ratios[-1],
+        'average_position_duration': total_days / len(positions.positions)
+    })
 
     return result_stats
