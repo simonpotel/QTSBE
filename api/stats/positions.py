6ba1499064d3469f7174329695d4ff815d51a5ff
simon <155122848+simonpotel@users.noreply.github.com>
2024-06-11 15:34:42 +0200

    Edit Stats Returns (API)
    - Removed Total Drawdowns (useless stats)
    - Removed Stability Ratio (useless)
    ~ Replaced max_loss stat for lowest_ratio
    + Added Biggest_ratio stat

--- api/stats/positions.py
+++ api/stats/positions.py
@@ -2,38 +2,48 @@ from datetime import datetime
 
 def get_position_stats(positions):
     result_stats = {
-        'max_loss': 0,
-        'max_loss_buy_index': 0,
-        'max_loss_sell_index': 0,
+        'lowest_ratio': float('inf'),
+        'lowest_ratio_buy_index': 0,
+        'lowest_ratio_sell_index': 0,
+        'biggest_ratio': float('-inf'),
+        'biggest_ratio_buy_index': 0,
+        'biggest_ratio_sell_index': 0,
         'average_ratio': 1,
-        'all_ratios': [0],
-        'cumulative_ratios': [0],
+        'all_ratios': [],
+        'cumulative_ratios': [],
         'max_cumulative_ratio': 1,
-        'daily_average_ratio': 1,
-        'hourly_average_ratio': 1,
         'average_position_duration': 0
     }
 
-    max_loss = float('-inf')
-    max_loss_buy_index = None
-    max_loss_sell_index = None
+    lowest_ratio = float('inf')
+    lowest_ratio_buy_index = None
+    lowest_ratio_sell_index = None
+    biggest_ratio = float('-inf')
+    biggest_ratio_buy_index = None
+    biggest_ratio_sell_index = None
     total_ratio = 0
     all_ratios = []
     cumulative_ratios = []
     max_cumulative_ratio = 1  # 1 (we start with no loss)
     total_days = 0
     total_positions = len(positions.positions)
-    if total_positions < 1 : return result_stats
-    for position in positions.positions:
-        loss = position['sell_price'] - position['buy_price']
-        if loss > max_loss:
-            max_loss = loss
-            max_loss_buy_index = position['buy_index']
-            max_loss_sell_index = position['sell_index']
+    if total_positions < 1:
+        return result_stats
 
+    for position in positions.positions:
         ratio = position['ratio']
         all_ratios.append(ratio)
         total_ratio += ratio
+
+        if ratio < lowest_ratio:
+            lowest_ratio = ratio
+            lowest_ratio_buy_index = position['buy_index']
+            lowest_ratio_sell_index = position['sell_index']
+
+        if ratio > biggest_ratio:
+            biggest_ratio = ratio
+            biggest_ratio_buy_index = position['buy_index']
+            biggest_ratio_sell_index = position['sell_index']
 
         if not cumulative_ratios:
             cumulative_ratios.append(ratio)
@@ -47,22 +57,18 @@ def get_position_stats(positions):
         total_days += duration.days
 
     average_ratio = total_ratio / total_positions
-
-    daily_average_ratio = average_ratio
-
-    hourly_average_ratio = daily_average_ratio / 24 
-
     average_position_duration = total_days / total_positions
 
-    result_stats['max_loss'] = max_loss
-    result_stats['max_loss_buy_index'] = max_loss_buy_index
-    result_stats['max_loss_sell_index'] = max_loss_sell_index
+    result_stats['lowest_ratio'] = lowest_ratio
+    result_stats['lowest_ratio_buy_index'] = lowest_ratio_buy_index
+    result_stats['lowest_ratio_sell_index'] = lowest_ratio_sell_index
+    result_stats['biggest_ratio'] = biggest_ratio
+    result_stats['biggest_ratio_buy_index'] = biggest_ratio_buy_index
+    result_stats['biggest_ratio_sell_index'] = biggest_ratio_sell_index
     result_stats['average_ratio'] = average_ratio
     result_stats['all_ratios'] = all_ratios
     result_stats['cumulative_ratios'] = cumulative_ratios
     result_stats['max_cumulative_ratio'] = max_cumulative_ratio
-    result_stats['daily_average_ratio'] = daily_average_ratio
-    result_stats['hourly_average_ratio'] = hourly_average_ratio
     result_stats['average_position_duration'] = average_position_duration
 
     return result_stats
