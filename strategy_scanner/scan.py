4f710c4c44c60f3ba9c0a04115c74b8a6f0b162c
simonpotel <potelsmn@gmail.com>
2024-06-22 15:00:38 +0200

    🧑‍💻 Edit list of common symbols for scanner

--- strategy_scanner/scan.py
+++ strategy_scanner/scan.py
@@ -1,34 +1,25 @@
 from BinanceScanner.scanner import BinanceScanner
-
 scanner = BinanceScanner()
 
-specific_symbols = ['BTC/USDT', 
-                    'ETH/USDT', 
-                    'SOL/USDT', 
-                    'DOGE/USDT',
-                    'BNB/USDT',
-                    'MATIC/USDT',
-                    'AVAX/USDT',
-                    'LUNA/USDT',
-                    'XRP/USDT',
-                    'LINK/USDT',
-                    'RNDR/USDT',
-                    'ADA/USDT',
-                    'FTM/USDT',
-                    'XTZ/USDT',
-                    'SUI/USDT',
-                    'SEI/USDT',
-                    'NEAR/USDT',
-                    'ARB/USDT',
-                    'GRT/USDT',
-                    'TRB/USDT',
-                    'BAND/USDT',
-                    'UNI/USDT',         
-                    'SHIB/USDT',
-                    'PEPE/USDT',
-                    'TRX/USDT']
+specific_symbols = [
+    'BTC/USDT', 'ETH/USDT', 'BNB/USDT',
+    'ADA/USDT', 'XRP/USDT', 'DOGE/USDT',
+    'LTC/USDT', 'DOT/USDT', 'UNI/USDT',
+    'LINK/USDT', 'BCH/USDT', 'LUNA/USDT',
+    'SOL/USDT', 'AVAX/USDT', 'ALGO/USDT',
+    'VET/USDT', 'FIL/USDT', 'ICP/USDT',
+    'MATIC/USDT', 'ATOM/USDT', 'XLM/USDT',
+    'TRX/USDT', 'ETC/USDT', 'FTT/USDT',
+    'THETA/USDT', 'XTZ/USDT', 'EOS/USDT',
+    'AAVE/USDT', 'KSM/USDT', 'NEO/USDT',
+    'MKR/USDT', 'CAKE/USDT', 'COMP/USDT',
+    'ENJ/USDT', 'ZEC/USDT', 'DASH/USDT',
+    'MANA/USDT', 'ZIL/USDT', 'CHZ/USDT',
+    'SAND/USDT', 'AXS/USDT'
+]
+
 timeframe = '1d'
-strategy = 'QTS_bollinger3_multi'
+strategy = 'default'
 fetch_latest_data = True
 
 scanner.scan(timeframe, strategy, fetch_latest_data, symbols=specific_symbols)
