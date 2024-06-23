1d2ddfaa8cd4a45452fe708afabbda2124381f84
simonpotel <potelsmn@gmail.com>
2024-06-23 19:22:43 +0200

    💥 parameters start_ts / end_ts in api request

--- api/api.py
+++ api/api.py
@@ -12,61 +12,64 @@ from algo.data.file import *
 from stats.drawdown import get_drawdowns_stats
 from stats.positions import get_position_stats
 
-
 debug_mode = True
-strategies_folder = r"api/strategies" # path to the folder containing strategy files
-strategies = {} # dictionary to store strategy functions
+strategies_folder = r"api/strategies"  # path to the folder containing strategy files
+strategies = {}  # dictionary to store strategy functions
 
 def reload_loguru_config():
     logger.remove()
-    logger.add(sys.stdout, level="DEBUG")  
+    logger.add(sys.stdout, level="DEBUG")
     if debug_mode:
         logger.add(r"logs/debug.log", level="DEBUG")
     else:
         logger.add(r"logs/logs.log", level="INFO")
+
 reload_loguru_config()
 
 def import_strategies():
-    for root, dirs, files in os.walk(strategies_folder): # loop through all files in the strategies folder and subfolders
+    for root, dirs, files in os.walk(strategies_folder):
         for file_name in files:
-            if file_name.endswith(".py"): # check if the file is a Python file
+            if file_name.endswith(".py"):
                 file_path = os.path.join(root, file_name)
                 name_without_extension = os.path.splitext(file_name)[0]
-                # create the strategy name with underscores for subdirectories
                 strategy_name = os.path.relpath(file_path, strategies_folder).replace(os.sep, '_').rsplit('.', 1)[0]
-                spec = importlib.util.spec_from_file_location(name_without_extension, file_path) # create a module specification
-                module = importlib.util.module_from_spec(spec) # create a module from the specification
-                spec.loader.exec_module(module) # load the module
-                strategies[strategy_name] = module # store the module in the dictionary
+                spec = importlib.util.spec_from_file_location(name_without_extension, file_path)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                strategies[strategy_name] = module
                 logger.debug(f"Content of {file_path} has been imported as module {strategy_name}")
-
 
 app = Flask(__name__)
 CORS(app, resources={r"/QTSBE/*": {"origins": "http://127.0.0.1"}})
 
 @app.route('/QTSBE/<pair>/<strategy>')
 def get_data(pair, strategy):
-    data = get_file_data(pair) # get data (list of lists) with datetime and price (in str)
-    prices = [float(entry[1].replace(',', ''))  for entry in data] # list of all prices in float 
-    result = strategies[strategy].analyse(data, prices) # call the strategy func
+    start_ts = request.args.get('start_ts')
+    end_ts = request.args.get('end_ts')
 
-    response = jsonify(
-        {"pair": pair, # string
-         "strategy": strategy, # string
-         "data": data, # tab of tabs of 2 elements
-         "result": (
-             result.indicators,
-             result.positions,
-             result.current_positions
-             ), # result is an object of the class Positions in stats/positions.py
-          "stats": 
-             {"drawdown:": get_drawdowns_stats(result),
-              "positions": get_position_stats(result)}
-        })
+    data = get_file_data(pair)  
+    prices = [float(entry[1].replace(',', '')) for entry in data]
+
+    result = strategies[strategy].analyse(data, prices, start_ts, end_ts)
+
+    response = jsonify({
+        "pair": pair,
+        "strategy": strategy,
+        "data": data,
+        "result": (
+            result.indicators,
+            result.positions,
+            result.current_positions
+        ),
+        "stats": {
+            "drawdown": get_drawdowns_stats(result),
+            "positions": get_position_stats(result)
+        }
+    })
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
 
-    logger.info(f"Request pair: {pair} | strategy: {strategy}")
+    logger.info(f"Request pair: {pair} | strategy: {strategy} | start_ts: {start_ts} | end_ts: {end_ts}")
     logger.debug(f"Request response: {response}")
     return response
 
