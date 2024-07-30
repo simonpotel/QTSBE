5f30bc85ec3603cb9bc6390fd7715124832bb90b
simon <155122848+simonpotel@users.noreply.github.com>
2024-07-30 18:23:32 +0200

    🐛 fix bug showing RSI plot structure when not needed

--- integrations/plotly/utils/plots.py
+++ integrations/plotly/utils/plots.py
@@ -50,47 +50,70 @@ def plot_json_data_in_gui(json_data, data_file, strategy):
     indicators = extract_indicators(json_data)
     trades = json_data['result'][1]
     trade_indices, trade_ratios = extract_trade_data(trades)
+
     rows = 1
     cols = 1
 
-    if 'RSI' or 'Normalize_MACD' in indicators:
-        price_row_height = 0.7
-        rsi_row_height = 0.3
+    if 'RSI' in indicators or 'Normalize_MACD' in indicators:
         rows += 1
+        rsi_row_height = 0.3
     else:
-        price_row_height = 1.0
         rsi_row_height = 0.0
 
-    row_heights=[price_row_height]
-    if 'RSI' or 'Normalize_MACD' in indicators:
-        row_heights.append(rsi_row_height)
+    if len(trade_ratios) > 0:
+        cols += 1
 
-    if len(trade_ratios) > 0: cols += 1
+    row_heights = [0.7]  
+    if rsi_row_height > 0:
+        row_heights.append(rsi_row_height)
 
     column_widths = [0.7] * cols
     if cols > 1:
         column_widths[0] = 0.7
 
-    fig = make_subplots(rows=rows, cols=cols, shared_xaxes=True, vertical_spacing=0.25,
-                        row_heights=row_heights,
-                        column_widths=column_widths)
+    fig = make_subplots(
+        rows=rows, cols=cols, 
+        shared_xaxes=True, 
+        vertical_spacing=0.25, 
+        row_heights=row_heights,
+        column_widths=column_widths
+    )
 
     fig.add_trace(go.Candlestick(
         x=dates, open=opens, high=highs, low=lows, close=closes,
         name="Price", 
-        increasing_line_color=chart_colors['increasing_line'], decreasing_line_color=chart_colors['decreasing_line'], 
-        increasing_fillcolor=chart_colors['increasing_fill'], decreasing_fillcolor=chart_colors['decreasing_fill']
+        increasing_line_color=chart_colors['increasing_line'], 
+        decreasing_line_color=chart_colors['decreasing_line'], 
+        increasing_fillcolor=chart_colors['increasing_fill'], 
+        decreasing_fillcolor=chart_colors['decreasing_fill']
     ), row=1, col=1)
 
     for indicator in indicators:
         row = 1
-        if indicator == 'RSI' or indicator == 'Normalize_MACD': row += 1
-        fig.add_trace(go.Scatter(x=dates, y=indicators[indicator], mode='lines', name=indicator, line=dict(color=chart_colors[indicator])), row=row, col=1)
+        if indicator == 'RSI' or indicator == 'Normalize_MACD':
+            row += 1
+        fig.add_trace(go.Scatter(
+            x=dates, y=indicators[indicator], 
+            mode='lines', 
+            name=indicator, 
+            line=dict(color=chart_colors[indicator])
+        ), row=row, col=1)
 
     if len(trade_ratios) > 0:
-        fig.add_trace(go.Scatter(x=trade_indices, y=trade_ratios, mode='lines', name='Trade Ratios', line=dict(color=chart_colors['Test'])), row=1, col=2)
+        fig.add_trace(go.Scatter(
+            x=trade_indices, y=trade_ratios, 
+            mode='lines', 
+            name='Trade Ratios', 
+            line=dict(color=chart_colors['Test'])
+        ), row=1, col=2)
+        
         cumulative_ratios = [float(cumulative_ratio) for cumulative_ratio in json_data["stats"]["positions"]["cumulative_ratios"]]
-        fig.add_trace(go.Scatter(x=trade_indices, y=cumulative_ratios, mode='lines', name='Cumulative Ratios', line=dict(color=chart_colors['MA_100'])), row=1, col=2)
+        fig.add_trace(go.Scatter(
+            x=trade_indices, y=cumulative_ratios, 
+            mode='lines', 
+            name='Cumulative Ratios', 
+            line=dict(color=chart_colors['MA_100'])
+        ), row=1, col=2)
 
     buy_dates = [trade['buy_date'] for trade in trades]
     buy_prices = [trade['buy_price'] for trade in trades]
@@ -104,11 +127,9 @@ def plot_json_data_in_gui(json_data, data_file, strategy):
 
     ratios = [float(ratio) for ratio in json_data["stats"]["positions"]["all_ratios"]]
 
-    # hover texts for the markets
     buy_hover_texts = [f"Index: {index}<br>Price: {price}<br>Date: {date}<br>Buy Signal: {buy_signal}" for index, price, date, buy_signal in zip(buy_indices, buy_prices, buy_dates, buy_signals)]
     sell_hover_texts = [f"Index: {index}<br>Price: {price}<br>Date: {date}<br>Ratio: {ratio}<br>Sell Signal: {sell_signal}" for index, price, date, ratio, sell_signal in zip(sell_indices, sell_prices, sell_dates, ratios, sell_signals)]
 
-    # plot buy/sell markes
     fig.add_trace(go.Scatter(
         x=buy_dates, 
         y=buy_prices, 
@@ -118,6 +139,7 @@ def plot_json_data_in_gui(json_data, data_file, strategy):
         hovertext=buy_hover_texts,  
         hoverinfo='text'  
     ), row=1, col=1)
+
     fig.add_trace(go.Scatter(
         x=sell_dates, 
         y=sell_prices, 
@@ -128,19 +150,21 @@ def plot_json_data_in_gui(json_data, data_file, strategy):
         hoverinfo='text' 
     ), row=1, col=1)
 
-    fig.update_layout(title=f"{data_file} ({strategy})",
-                      xaxis_title='Date',
-                      yaxis_title='Price',
-                      xaxis_rangeslider_visible=False,
-                      plot_bgcolor=chart_colors['Background'],
-                      paper_bgcolor=chart_colors['Background'],
-                      font=dict(color="black" if theme == 'white' else "white"),
-                      yaxis=dict(gridcolor=chart_colors['Background']),
-                      xaxis=dict(gridcolor=chart_colors['Background']),
-                      yaxis2=dict(gridcolor=chart_colors['Background']),
-                      xaxis2=dict(gridcolor=chart_colors['Background']))
+    fig.update_layout(
+        title=f"{data_file} ({strategy})",
+        xaxis_title='Date',
+        yaxis_title='Price',
+        xaxis_rangeslider_visible=False,
+        plot_bgcolor=chart_colors['Background'],
+        paper_bgcolor=chart_colors['Background'],
+        font=dict(color="black" if theme == 'white' else "white"),
+        yaxis=dict(gridcolor=chart_colors['Background']),
+        xaxis=dict(gridcolor=chart_colors['Background']),
+        yaxis2=dict(gridcolor=chart_colors['Background']),
+        xaxis2=dict(gridcolor=chart_colors['Background'])
+    )
 
-    if 'RSI' or 'Normalize_MACD' in indicators:
+    if 'RSI' in indicators or 'Normalize_MACD' in indicators:
         fig.update_yaxes(range=[0, 100], row=2, col=1)
         fig.add_shape(type="line", x0=min(dates), y0=50, x1=max(dates), y1=50, row=2, col=1, line=dict(color="LightSkyBlue", width=3))
 
@@ -150,4 +174,3 @@ def plot_json_data_in_gui(json_data, data_file, strategy):
     os.makedirs(directory, exist_ok=True)
     plot_filename = f'plot_{data_file}_{strategy}.html'
     fig.write_html(directory + plot_filename)
-    #webbrowser.open(os.path.join(os.getcwd(), 'integrations', 'plotly', 'saved_results', plot_filename)) # save html file
