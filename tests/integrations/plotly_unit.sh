f8e78cec2e8f53188d3f3a3ae61efc32c43a2387
simon <155122848+simonpotel@users.noreply.github.com>
2024-11-09 00:18:32 +0100

    Update plotly_unit.sh

--- tests/integrations/plotly_unit.sh
+++ tests/integrations/plotly_unit.sh
@@ -1,3 +1,3 @@
 python integrations/plotly/main.py -strategy default -data Binance_BTCUSDT_1d 
 python integrations/plotly/main.py -strategy rsi_example -data Binance_BTCUSDT_1d -multi_positions False
-python integrations/plotly/main.py -strategy rsi_example -data Binance_BTCUSDT_1d -start_ts 2020-01-01 -end_ts 2021-01-10 -multi_positions False
+python integrations/plotly/main.py -strategy rsi_example -data Binance_BTCUSDT_1d -start_ts '2020-01-01 00:00:00' -end_ts '2021-01-10 00:00:00' -multi_positions False
