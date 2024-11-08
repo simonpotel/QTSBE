12e2f3f9ef5424cdff2b2606340401348b4b4e88
simon <155122848+simonpotel@users.noreply.github.com>
2024-11-08 23:08:18 +0100

    class Indicator for each strategy

--- api/strategies/default.py
+++ api/strategies/default.py
@@ -1,43 +1,11 @@
-from algo.indicators.ma import get_MA
-from algo.indicators.rsi import get_RSI
-from algo.indicators.macd import get_MACD
-from algo.indicators.ema import get_EMA
-from algo.indicators.normalize_macd import get_normalize_MACD
-from algo.indicators.bollinger_bands import get_bollinger_bands
-from stats.trades import Positions
-from api import logger
+class Indicators(object):
+    def __init__(self, data):
+        self.data = data
+        self.indicators = {}
 
-def analyse(data, prices, start_ts, end_ts, multi_positions):
-    positions = Positions()
+def buy_signal(open_position, data, index_check, indicators):
+    return 0
 
-    # indicators used in this strategy
-    ma_20            = get_MA(prices, 20)
-    ma_40            = get_MA(prices, 40)
-    ma_100           = get_MA(prices, 100)
-    rsi              = get_RSI(prices, 14)
-    macd, ema_macd   = get_MACD(prices)
-    normalize_macd   = get_normalize_MACD(macd)
-    bollinger_upper, bollinger_rolling, bollinger_lower = get_bollinger_bands(prices)
 
-    positions.indicators = {  # must convert all to a list because its using np (Object of type ndarray is not JSON serializable)
-        "MA_20": list(ma_20),
-        "MA_40": list(ma_40),
-        "MA_100": list(ma_100),
-        "RSI": list(rsi),
-        "MACD": list(macd),
-        "EMA_MACD": list(ema_macd),
-        "Normalize_MACD": list(normalize_macd),
-        "Bollinger_Lower": list(bollinger_lower),
-        "Bollinger_Rolling": list(bollinger_rolling),
-        "Bollinger_Upper": list(bollinger_upper),
-    }
-
-    #for i in range(len(prices) - 1):  # iterate through each price entry
-        #if indicator[i] is None: continue  # no data on a indicator (for the first data of prices)
-
-        #if positions.current_position == {}: # not already in a position
-            #
-        #else:
-            #
-
-    return positions
+def sell_signal(open_position, data, index_check, indicators):
+    return 0
--- api/strategies/rsi_example.py
+++ api/strategies/rsi_example.py
@@ -1,7 +1,47 @@
-from algo.indicators.rsi import get_RSI
-from stats.trades import Positions
-from datetime import datetime
-#from api import logger
+import numpy as np
+
+# Article to understand the RSI : https://admiralmarkets.com/fr/formation/articles/indicateurs-forex/indicateur-rsi
+
+def get_RSI(prices, window=14):
+    """
+    Calculate the Relative Strength Index (RSI) for a given price series.
+
+    Parameters:
+    prices (list or array-like): List or array of price data.
+    window (int): The window size for the RSI calculation. Default is 14.
+
+    Returns:
+    numpy.ndarray: An array containing the RSI values for the given price series. (NB: You must convert it to use it in a Python List)
+    """
+    
+    deltas = np.diff(prices) # tab of price differences between consecutive days
+    seed = deltas[:window+1]
+    
+    # caverage gain and loss over the window period
+    up = seed[seed >= 0].sum() / window
+    down = -seed[seed < 0].sum() / window
+    
+    rs = up / down # calculate the initial Relative Strength (RS)
+    rsi = np.zeros_like(prices) # create the RSI array with zeros, the same length as prices
+    rsi[:window] = 100. - 100. / (1. + rs) # set the first window RSI values using the initial RS calculation
+    for i in range(window, len(prices)): # calculate RSI for the rest of the prices using a iterrative while
+        delta = deltas[i - 1]  # get the price change for the current period
+        # determine the gain (upval) and loss (downval) for the current period
+        if delta > 0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+        # update the average gain and loss with the current values
+        up = (up * (window - 1) + upval) / window
+        down = (down * (window - 1) + downval) / window
+
+        rs = up / down
+        rsi[i] = 100. - 100. / (1. + rs)
+
+    return rsi
+
 
 # This example of analysis shows you 
 # how you can use an indicator along with classes and functions of QTSBE
@@ -12,47 +52,33 @@ from datetime import datetime
 # This is why, if you run this example on BTCUSDT since 2018, 
 # you will only have 20 transactions and only a 3x increase in your capital over 6 years. 
 
-def analyse(data, prices, start_ts, end_ts, multi_positions):
-    positions = Positions()  
-    ts_format = "%Y-%m-%d"
+class Indicators(object):
+    def __init__(self, data):
+        self.data = data
+        self.indicators = self.calculate_indicators()
 
-    # indicators used in this strategy example
-    rsi = get_RSI(prices, 14)
+    def calculate_indicators(self):
+        data_open = [row[1] for row in self.data]
 
-    positions.indicators = {  # must convert all to a list because its using np (Object of type ndarray is not JSON serializable)
-        "RSI": list(rsi)
-    }
+        indicators = {
+            "RSI": get_RSI(data_open),
+        }
+        return {k: list(v) for k, v in indicators.items()}
 
-    for i in range(len(prices) - 1):  # iterate through each price entry
-        if rsi[i] is None: 
-            continue  # no data on RSI (for the first data of prices)
-        if (start_ts and start_ts > datetime.strptime(data[i][0], ts_format)) or (end_ts and end_ts < datetime.strptime(data[i][0], ts_format)):
-            continue 
 
-        if  (multi_positions == False and len(positions.current_positions) == 0) or multi_positions == True:
-            if rsi[i] < 40:  # RSI is less than 40, buy signal
-                positions.add_position(
-                    buy_index=i,
-                    buy_price=prices[i],
-                    buy_date=data[i][0],
-                    buy_signals={
-                        "RSI": rsi[i],
-                        "Buy_Signal": 1
-                    }
-                )
+def buy_signal(open_position, data, index_check, indicators):
+    if indicators["RSI"][index_check] is None:
+        return -2
+    if indicators["RSI"][index_check] < 40:
+        return 1
+    return -2
 
-        # iterate over a copy of current_positions to avoid modification during iteration
-        for position in positions.current_positions[:]:  
-            if rsi[i] > 50 and prices[i] / position['buy_price'] > 1.10:  # RSI > 50 and simple check ratio (⚠️ Don't do this)
-                positions.close_position(
-                    buy_index=position['buy_index'],
-                    sell_index=i,
-                    sell_price=prices[i],
-                    sell_date=data[i][0],
-                    sell_signals={
-                        "RSI": rsi[i],
-                        "Sell_Signal": 1
-                    }
-                )
 
-    return positions
+def sell_signal(open_position, data, index_check, indicators):
+    if indicators["RSI"][index_check] is None:
+        return -1
+    if open_position.get('buy_signal') == 1 or open_position.get('buy_signals', {}).get('Buy_Signal') == 1:
+        if open_position['buy_index'] < index_check < len(data):
+            if indicators["RSI"][index_check] > 50 and data[index_check][2] / open_position['buy_price'] > 1.10:
+                return 1
+    return -1
